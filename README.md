50af461a3063b4a6eaa8d80850dd9417078a4f0b
David Weinstein <weinstein.dev@gmail.com>
2019-08-21 22:16:12 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# the-odin-project
+ working through the odin project
---
+++ project-1.rb
@@ -0,0 +1,17 @@
+# The methodbelow works for an all lower case, all alpha string, but fails when upcase or " " or ! is put in. Will have to research this instead of jumping right in.
+
+def caesar_cipher(string, num)
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    new_string = ""
+    string.each_char do |char|
+        old_idx = alphabet.index(char)
+        new_idx = old_idx + num
+        new_char = alphabet[new_idx % 26]
+        new_string += new_char
+    end
+    return new_string
+end
+
+
+print caesar_cipher("What a string!", 5)
+puts
